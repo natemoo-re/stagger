3c455df63d4ae7ec0f133bfcd445d52f3fa5e5a5
natemoo-re <ntmoore@knox.edu>
2019-07-19 22:58:18 -0500

    Add demo

---
+++ examples/basic/index.ts
@@ -0,0 +1,11 @@
+import stagger from 'stagger/core';
+
+const h1 = document.querySelector('h1')!;
+const list = document.querySelector<HTMLElement>('.list')!;
+
+// console.log(dom.isText(h1));
+// console.log(dom.isText(list));
+
+// console.log(dom.children(list));
+
+stagger({ delay: 100 });
